88f9ff7599aca2cf27f4057bcdb022b6e6c857e3
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-10-04 21:14:07 +0800

    fix baseamasiro

--- plugin/baseamasiro/main.go
+++ plugin/baseamasiro/main.go
@@ -14,7 +14,7 @@ import (
 )
 
 func init() {
-	en := rei.Register("base64gua", &ctrl.Options[*rei.Ctx]{
+	en := rei.Register("baseamasiro", &ctrl.Options[*rei.Ctx]{
 		DisableOnDefault: false,
 		Help: "base天城文加解密\n" +
 			"- 天城文加密xxx\n- 天城文解密xxx\n- 天城文用yyy加密xxx\n- 天城文用yyy解密xxx",
