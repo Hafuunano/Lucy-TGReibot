0f67e2e40bc7d673c2a3973f5e44dcb4a86a1cbe
源文雨 <41315874+fumiama@users.noreply.github.com>
2022-10-05 16:50:26 +0800

    nv add 发送原图

--- plugin/novelai/main.go
+++ plugin/novelai/main.go
@@ -51,12 +51,54 @@ func init() {
 				return
 			}
 			seedtext := strconv.Itoa(seed)
-			_, _ = ctx.SendPhoto(
-				tgba.FileBytes{Bytes: img},
-				true, "seed: "+seedtext+"\ntags: "+tags,
-				tgba.MessageEntity{Type: "bold", Offset: 0, Length: 5},
-				tgba.MessageEntity{Type: "bold", Offset: 5 + 1 + len(seedtext) + 1, Length: 5},
-			)
+			fn := tags + " " + seedtext
+			err = os.WriteFile(en.DataFolder()+fn+".png", img, 0755)
+			if err != nil {
+				_, _ = ctx.SendPlainMessage(false, "ERROR: ", err)
+				return
+			}
+			_, _ = ctx.Caller.Send(&tgba.PhotoConfig{
+				BaseFile: tgba.BaseFile{
+					BaseChat: tgba.BaseChat{
+						ChatID:           ctx.Message.Chat.ID,
+						ReplyToMessageID: ctx.Message.MessageID,
+						ReplyMarkup: tgba.NewInlineKeyboardMarkup(
+							tgba.NewInlineKeyboardRow(
+								tgba.NewInlineKeyboardButtonData(
+									"发送原图",
+									"nvaiorg"+fn,
+								),
+							),
+						),
+					},
+					File: tgba.FileBytes{Bytes: img},
+				},
+				Caption: "seed: " + seedtext + "\ntags: " + tags,
+				CaptionEntities: []tgba.MessageEntity{
+					{Type: "bold", Offset: 0, Length: 5},
+					{Type: "bold", Offset: 5 + 1 + len(seedtext) + 1, Length: 5},
+				},
+			})
+		})
+	en.OnCallbackQueryRegex(`^nvaiorg([0-9A-Za-z_\s]+\d+)$`).SetBlock(true).
+		Handle(func(ctx *rei.Ctx) {
+			if len(ctx.Message.ReplyMarkup.InlineKeyboard) > 0 {
+				_, _ = ctx.Caller.Send(&tgba.EditMessageReplyMarkupConfig{
+					BaseEdit: tgba.BaseEdit{
+						ChatID:    ctx.Message.Chat.ID,
+						MessageID: ctx.Message.MessageID,
+					},
+				})
+			}
+			fn := ctx.State["regex_matched"].([]string)[1]
+			f := tgba.NewDocument(ctx.Message.Chat.ID, tgba.FilePath(en.DataFolder()+fn+".png"))
+			f.ReplyToMessageID = ctx.Message.MessageID
+			_, err := ctx.Caller.Send(&f)
+			if err != nil {
+				_, _ = ctx.Caller.Send(tgba.NewCallbackWithAlert(ctx.Value.(*tgba.CallbackQuery).ID, "ERROR: "+err.Error()))
+				return
+			}
+			_, _ = ctx.Caller.Send(tgba.NewCallbackWithAlert(ctx.Value.(*tgba.CallbackQuery).ID, "已发送"))
 		})
 	en.OnMessageRegex(`^设置\s?novelai\s?key\s?([0-9A-Za-z_]{64})$`, rei.SuperUserPermission, rei.OnlyPrivate).SetBlock(true).
 		Handle(func(ctx *rei.Ctx) {
