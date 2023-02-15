6b1d0a1819ae98daa7020d75b03262e66323bc04
源文雨 <41315874+fumiama@users.noreply.github.com>
2023-02-15 20:50:07 +0800

    v0.1.6

--- kanban/banner.go
+++ kanban/banner.go
@@ -1,26 +1,10 @@
 package kanban
 
-import (
-	"fmt"
-	"strings"
-)
+// Version ...
+var Version = "v0.1.6"
 
-var (
-	info = [...]string{
-		"* Telegram + ReiBot + Golang",
-		"* Version 0.1.5 - 2022-10-13 11:52:55 +0800 CST",
-		"* Copyright © 2020 - 2022 FloatTech. All Rights Reserved.",
-		"* Project: https://github.com/FloatTech/ReiBot-Plugin",
-	}
-	// Banner ...
-	Banner = strings.Join(info[:], "\n")
-)
-
-// PrintBanner ...
-func PrintBanner() {
-	fmt.Print(
-		"\n======================[ReiBot-Plugin]======================",
-		"\n", Banner, "\n",
-		"===========================================================\n\n",
-	)
-}
+// Banner ...
+var Banner = "* Telegram + ReiBot + Golang\n" +
+	"* Version " + Version + " - 2023-02-15 20:49:54 +0800 CST\n" +
+	"* Copyright © 2020 - 2023 FloatTech. All Rights Reserved.\n" +
+	"* Project: https://github.com/FloatTech/ReiBot-Plugin"
--- kanban/init.go
+++ kanban/init.go
@@ -1,6 +1,18 @@
 // Package kanban 打印版本信息
 package kanban
 
+import "fmt"
+
+//go:generate go run github.com/FloatTech/ReiBot-Plugin/kanban/gen
+
 func init() {
 	PrintBanner()
+}
+
+func PrintBanner() {
+	fmt.Print(
+		"\n======================[ReiBot-Plugin]======================",
+		"\n", Banner, "\n",
+		"===========================================================\n\n",
+	)
 }
