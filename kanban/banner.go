a80f75f9772eac3b9ee19a246af44d7d903d034d
MoYoez <i@himoyo.cn>
2023-11-18 21:35:08 +0800

    add manual book

--- kanban/banner.go
+++ kanban/banner.go
@@ -4,4 +4,5 @@ package kanban
 var Banner = "* Telegram + ReiBot \n" +
 	"* Unofficial Fork of Lucy (HiMoYo Bot.)\n" +
 	"* Copyright © 2020 - 2023 FloatTech. All Rights Reserved.\n" +
-	"* Project: https://github.com/MoYoez/ReiBot-Plugin"
+	"* ManualBook : https://side-lucy.lemonkoi.one \n" +
+	"* Project: https://github.com/MoYoez/ReiBot-Plugin "
